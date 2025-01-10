b10fdc599057a122ff5e1ab2a2eeba5ad402cea5
tdu7 <126829455+tdu7@users.noreply.github.com>
2025-01-10 11:45:03 +0100

    initialisation

---
+++ src/main/java/com/example/Magazine_Reservation/MagazineReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Magazine_Reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineReservationApplication.class, args);
+	}
+
+}
